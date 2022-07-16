73e1c4951a551a1528c7d8d5dc9a999ed6cb82b4
Vitaliy Biryuckov <stukov.biryuckov2017@yandex.ru>
2022-07-16 16:31:21 +0800

    Init commit

---
+++ src/main/java/ru/isu/math/config/DispatcherServletInitializer.java
@@ -0,0 +1,20 @@
+package ru.isu.math.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class DispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {MVCConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
