dea911740aa24570a62ae27064bdd5158be5015d
Vitaliy Biryuckov <stukov.biryuckov2017@yandex.ru>
2022-07-22 19:15:35 +0800

    add security config and in-memory users

---
+++ src/main/java/ru/isu/math/config/security/SecurityInit.java
@@ -0,0 +1,6 @@
+package ru.isu.math.config.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
